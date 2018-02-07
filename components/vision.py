7391564050e177275e4de30da4c82f6696a31a97
Injector-Spenral <sam.richards@thedropbears.org.au>
2018-02-07 19:09:44 +1100

    added stuff and documented

--- components/vision.py
+++ components/vision.py
@@ -1,13 +1,14 @@
-import time
 import numpy as np
 from networktables import NetworkTables
+from time import monotonic
+
 
 
 class Vision:
 
     def setup(self):
         """This is called after variables are injected by magicbot."""
-        self.nt = NetworkTables
+        self.nt = NetworkTables 
         self.sd = self.nt.getTable("/vision")
         self.entry = self.sd.getEntry("info")
         self.entry.addListener(self.new_value, self.nt.NotifyFlags.UPDATE)
@@ -27,7 +28,7 @@ class Vision:
         pass
 
     def new_value(self, entry, key, value, param):
-        start_time = time.time()
+        start_time = monotonic() #saves the current monotonic time
         if key == "/vision/info":
             self.unpack_info(value, start_time)
 
@@ -39,18 +40,19 @@ class Vision:
         return out
 
     def unpack_info(self, info, start_time):
-        if len(info) <= 1:
+        if len(info) <= 1: 
             self.vision = [None]
             return
-
         info = np.array(info)
-        info = self.format_list(info, 3)
-        total_time = (time.time() - start_time) + float(info[-1])
-        info[-1] = total_time
+        info = self.format_list(info, 2)
+        processing_time = (monotonic() - start_time) + float(info[-1]) 
+
+        info[-1] = [processing_time, start_time]
 
         self.vision = info
 
-    def largest_cube(self):
+    def largest_cube(self): 
+        """Checks if the info list has two items. If not return none(a cube has not been detected), if so return the x angle"""
         if len(self.vision) < 2:
             return None
-        return self.vision[0][0]
+        return self.vision[0][0] 
